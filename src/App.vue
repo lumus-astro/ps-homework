28e59c0beb40bc9b5ed20dc95a0808471217c471
lumus-astro <lumus-astro@lumus.dev>
2025-05-08 01:46:10 +0500

    ДЗ #3

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,46 @@
 <script setup>
 import Button from "./components/Button.vue";
+import Toolbar from "./components/Toolbar.vue";
+import Card from "./components/Card.vue";
 </script>
 
 <template>
   <div class="page">
-    <Button>
-      <template #default>Начать игру</template>
-    </Button>
+    <div class="header">
+      <Toolbar/>
+    </div>
+
+    <div class="content">
+      <Button>
+        <template #default>Начать игру</template>
+      </Button>
+
+      <Card/>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .page {
+  display: flex;
+  flex-direction: column;
   height: 100vh;
   width: 100vw;
+  background-color: var(--color-bg-main);
+}
+
+.header {
+  height: 120px;
+  padding: 0 60px;
   display: flex;
+  align-items: center;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
   justify-content: center;
   align-items: center;
-  background-color: var(--color-bg-main);
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,68 @@
+<script setup>
+
+const emit = defineEmits(["flip", "change-status"])
+</script>
+
+<template>
+  <div class="card">
+    <div class="inner">
+      <span class="number">02</span>
+      <span class="text">vino</span>
+      <button class="button" @click="emit('flip')">Перевернуть</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 250px;
+  min-height: 376px;
+
+  border-radius: 16px;
+  box-shadow: 0 0 16px 0 rgba(0, 0, 0, 0.1);
+  background: var(--color-card-bg);
+
+  padding: 28px 19px;
+}
+
+.inner {
+  height: 100%;
+  border: 1px solid var(--color-card-border);
+  border-radius: 12px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.number {
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 16px;
+  position: absolute;
+  top: -7px;
+  left: 16px;
+  display: block;
+  background: var(--color-card-bg);
+}
+
+.text {
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 21px;
+}
+
+.button {
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 18px;
+  background: var(--color-card-bg);
+  display: block;
+  bottom: -11px;
+  left: 25%;
+}
+</style>
